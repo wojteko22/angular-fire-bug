52af9254299145a09302b976e8b90c63562731af
Wojciech Okoński <wojteko22.96@gmail.com>
2022-08-09 08:38:57 +0000

    Import AngularFireDatabaseModule (error)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database"; // todo: Source of error
+import { AngularFireAuthModule } from "@angular/fire/compat/auth"; // todo: This is OK
 
 @NgModule({
   declarations: [
